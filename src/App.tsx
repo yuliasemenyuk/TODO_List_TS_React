80208df581bcfebe8cbf249654fc22b1679c8315
yuliasemenyuk <semenyukyp@gmail.com>
2024-10-28 21:06:51 +0200

    add todo ul

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  const todos = [
+    { id: "t1", text: "Finish the cource" },
+    { id: "t2", text: "Be happy" },
+  ];
+  return (
+    <div className="App">
+      <TodoList items={todos}/>
+    </div>
+  );
 }
 
 export default App;
