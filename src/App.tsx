79db278bfdc9da71d50565ee3bf789a86624583d
yuliasemenyuk <semenyukyp@gmail.com>
2024-10-28 21:25:16 +0200

    new todo handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 function App() {
   const todos = [
     { id: "t1", text: "Finish the cource" },
     { id: "t2", text: "Be happy" },
   ];
+
+  const todoAddHandler = (text:string) => {
+    console.log(text)
+  }
+
   return (
     <div className="App">
+      <NewTodo onAddTodo={todoAddHandler}/>
       <TodoList items={todos}/>
     </div>
   );
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,25 @@
+import React, { useRef } from "react";
+
+interface NewTodoProps {
+  onAddTodo: (todoText: string) => void;
+}
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+  const todoSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    props.onAddTodo(enteredText);
+  };
+  return (
+    <form onSubmit={todoSubmitHandler}>
+      <div>
+        <label htmlFor="todo-text">Todo Text</label>
+        <input type="text" id="todo-text" ref={textInputRef} />
+        <button type="submit">ADD TODO</button>
+      </div>
+    </form>
+  );
+};
+
+export default NewTodo;
